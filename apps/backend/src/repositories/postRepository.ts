6aa3ef880ad13d525d0071217407f791a7267353
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-10-07 19:53:56 +0800

    feat(likes): fetching posts now include likes count

--- apps/backend/src/repositories/postRepository.ts
+++ apps/backend/src/repositories/postRepository.ts
@@ -3,8 +3,27 @@ import { Post } from '../types/post';
 
 const postRepository = {
   create: (data: Post) => prisma.post.create({ data }),
-  findById: (id: number) => prisma.post.findUnique({ where: { id } }),
-  findAll: () => prisma.post.findMany(),
+  findById: (id: number) =>
+    prisma.post.findUnique({
+      where: { id },
+      include: {
+        _count: {
+          select: {
+            Like: true,
+          },
+        },
+      },
+    }),
+  findAll: () =>
+    prisma.post.findMany({
+      include: {
+        _count: {
+          select: {
+            Like: true,
+          },
+        },
+      },
+    }),
   deleteById: (id: number) => prisma.post.delete({ where: { id } }),
 };
 
