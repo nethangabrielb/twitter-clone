106608d47ded408cac290f921ef1121516b2fc6b
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-08 00:33:12 +0800

    feat: registration body zod schema

---
+++ apps/frontend/src/app/register/schemas/registerSchema.ts
@@ -0,0 +1,61 @@
+import * as z from "zod";
+
+import { authApi } from "@/lib/api/auth";
+
+export const RegisterSchema = z
+  .object({
+    name: z.string().refine((val) => val.trim().length >= 1, {
+      message: "Name is required",
+    }),
+    username: z
+      .string()
+      .refine((val) => val.trim().length >= 1, {
+        message: "Username is required",
+      })
+      .refine(
+        async (val) => {
+          const isUnique = await authApi.checkPropertyUnique(val, "username");
+          return isUnique;
+        },
+        { message: "This username is already taken" },
+      ),
+    email: z
+      .email({ error: "Must be a valid email" })
+      .refine((val) => val.trim().length >= 1, {
+        message: "Email is required",
+      })
+      .refine(
+        async (val) => {
+          const isUnique = await authApi.checkPropertyUnique(val, "email");
+          return isUnique;
+        },
+        { message: "This email is already taken" },
+      ),
+    password: z
+      .string()
+      .refine((val) => val.trim().length >= 1, {
+        error: "Password is required",
+      })
+      .min(12, { error: " " })
+      .refine((val) => /[A-Z]/.test(val), {
+        error: "one capital",
+      })
+      .refine((val) => /[^A-Za-z0-9]/.test(val), {
+        error: "special char",
+      }),
+    confirmPassword: z
+      .string()
+      .refine((val) => val.trim().length >= 1, {
+        error: "Password is required",
+      })
+      .min(12, { error: " " })
+      .refine((val) => /[A-Z]/.test(val), {
+        error: "one capital",
+      })
+      .refine((val) => /[^A-Za-z0-9]/.test(val), {
+        error: "special char",
+      }),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    path: ["confirmPassword"],
+  });
