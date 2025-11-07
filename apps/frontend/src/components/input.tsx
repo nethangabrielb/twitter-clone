f04da7afb55e47088b53bee77815865b31faeeb7
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-08 00:32:50 +0800

    refactor(ui): input component to be more flexible

--- apps/frontend/src/components/input.tsx
+++ apps/frontend/src/components/input.tsx
@@ -1,29 +1,57 @@
 import { ConfirmUser } from "@/app/onboarding/components/form";
+import { Register } from "@/app/register/components/form";
 import _ from "lodash";
-import type { UseFormRegister } from "react-hook-form";
+import type { Path, UseFormRegister } from "react-hook-form";
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-type Props = {
-  children: string;
-  label: "avatar" | "username" | "name";
-  register: UseFormRegister<ConfirmUser>;
-  errorMessage: string | undefined;
+import { cn } from "@/lib/utils";
+
+type Props<T extends Register | ConfirmUser> = {
+  children?: string;
+  label: keyof T;
+  register: UseFormRegister<T>;
+  errorMessage?: string;
+  displayErrMessage?: boolean;
 };
 
-const InputSharp = ({ children, label, register, errorMessage }: Props) => {
+const InputSharp = <T extends Register | ConfirmUser>({
+  children,
+  label,
+  register,
+  errorMessage,
+  displayErrMessage = true,
+}: Props<T>) => {
   return (
     <div className="flex flex-col gap-2 relative">
-      <Label>{_.upperFirst(label)}</Label>
-      <Input
-        defaultValue={children}
-        {...register(label)}
-        className="active:border-primary py-6 rounded-[4px] bg-transparent border border-border"
-        required
-      ></Input>
-      <p className="text-red-600 font-medium text-xs absolute top-0 right-0 mb-2">
-        {errorMessage}
+      <Label className="font-bold text-lg tracking-tighter">
+        {_.upperFirst(label as string)}
+      </Label>
+      {children ? (
+        <Input
+          defaultValue={children}
+          {...register(label as Path<T>)}
+          className={cn(
+            "active:border-primary py-6 rounded-[4px] bg-transparent border border-border",
+            errorMessage &&
+              "border-red-400  focus:border-red-400! focus-visible:ring-red-500",
+          )}
+          required
+        ></Input>
+      ) : (
+        <Input
+          {...register(label as Path<T>)}
+          className={cn(
+            "active:border-primary py-6 rounded-[4px] bg-transparent border border-border",
+            errorMessage &&
+              "border-red-400  focus:border-red-400! focus-visible:ring-red-500",
+          )}
+          required
+        ></Input>
+      )}
+      <p className="text-red-600 font-medium text-xs absolute top-0 right-0 mt-4">
+        {displayErrMessage && errorMessage}
       </p>
     </div>
   );
