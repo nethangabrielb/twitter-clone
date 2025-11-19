b2272dc4a7f0bf34a8916563653e0a7c19bcd109
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-19 22:57:51 +0800

    feat: styled the initial post in feed

--- apps/frontend/src/app/home/components/feed-post.tsx
+++ apps/frontend/src/app/home/components/feed-post.tsx
@@ -8,14 +8,22 @@ type Props = {
 
 const FeedPost = ({ post }: Props) => {
   return (
-    <div className="flex p-4 border-b border-b-border">
-      <img src={post.user.avatar} alt="User icon" width={48} height={48} />
+    <div className="flex gap-4 p-4 border-b border-b-border">
+      <img
+        src={post.user.avatar}
+        alt="User icon"
+        className="rounded-full object-cover size-12"
+      />
       <div className="flex flex-col gap-3">
-        <div className="flex gap-1">
-          <p>{post.user.name}</p>
-          <p>@{post.user.username}</p>
+        <div className="flex gap-2 items-center">
+          <p className="font-bold text-text space tracking-[0.2px] text-[18px]">
+            {post.user.name}
+          </p>
+          <p className="text-darker font-light text-[15px]">
+            @{post.user.username}
+          </p>
         </div>
-        <p>{post.content}</p>
+        <p className="text-text text-[15px]">{post.content}</p>
       </div>
     </div>
   );
