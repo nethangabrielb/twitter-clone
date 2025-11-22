140ad4aa0d7408f52005bd1799bab418417d2f17
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-22 13:10:30 +0800

    feat: format and render date post is created

--- apps/frontend/src/app/home/components/feed-post.tsx
+++ apps/frontend/src/app/home/components/feed-post.tsx
@@ -2,6 +2,8 @@
 
 import { Heart, MessageCircle } from "lucide-react";
 
+import { formatDate } from "@/lib/utils";
+
 import { Post } from "@/types/post";
 
 type Props = {
@@ -9,7 +11,6 @@ type Props = {
 };
 
 const FeedPost = ({ post }: Props) => {
-  console.log(post);
   return (
     <div className="flex gap-4 p-4 border-b border-b-border">
       <img
@@ -17,14 +18,22 @@ const FeedPost = ({ post }: Props) => {
         alt="User icon"
         className="rounded-full object-cover size-12"
       />
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-2 items-center">
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-1 items-center">
           <p className="font-bold text-text space tracking-[0.2px] text-[18px]">
             {post.user.name}
           </p>
-          <p className="text-darker font-light text-[15px]">
-            @{post.user.username}
-          </p>
+          <div className="flex items-center gap-1">
+            <p className="text-darker font-light text-[15px]">
+              @{post.user.username}
+            </p>
+            <div className="text-darker font-light w-0.8 my-auto flex justify-center text-a items-center">
+              .
+            </div>
+            <p className="text-darker font-light text-[14px]">
+              {formatDate(post.createdAt)}
+            </p>
+          </div>
         </div>
         <p className="text-text text-[15px]">{post.content}</p>
         <div className="flex justify-start">
