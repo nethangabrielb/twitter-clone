64e49cd2def4c62ae418f475b6f9c3f1ce735e48
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-23 21:27:34 +0800

    feat(ui): added control buttons for feed content

--- apps/frontend/src/app/home/page.tsx
+++ apps/frontend/src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import CreatePost from "@/app/home/components/create-post";
 import FeedPost from "@/app/home/components/feed-post";
 
 import postApi from "@/lib/api/post";
@@ -8,7 +9,13 @@ const Home = async () => {
   const posts: Post[] = await postApi.getPosts();
 
   return (
-    <div className="lg:w-[600px] border-l border-r border-l-border border-r-border h-full">
+    <div className="lg:w-[600px] border-l border-r border-l-border border-r-border h-full relative">
+      <div className="flex backdrop-blur-lg absolute top-0 w-full">
+        <FeedControlBtn>For you</FeedControlBtn>
+        <FeedControlBtn>Following</FeedControlBtn>
+      </div>
+      <div className="mt-[57.1px]"></div>
+      <CreatePost></CreatePost>
       {/* RENDER POSTS */}
       <div className="w-full">
         {posts.map((post: Post) => {
@@ -16,6 +23,14 @@ const Home = async () => {
         })}
       </div>
     </div>
+  );
+};
+
+const FeedControlBtn = ({ children }: { children: string }) => {
+  return (
+    <button className="bg-transparent flex-1 p-4 hover:bg-neutral-900 border-b border-b-border ">
+      {children}
+    </button>
   );
 };
 
