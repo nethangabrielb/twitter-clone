d545f9955d50a857685f535f7e9958e9253c6b5a
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-11-27 17:37:46 +0800

    feat: added page title on post slug page

--- apps/frontend/src/app/post/[id]/page.tsx
+++ apps/frontend/src/app/post/[id]/page.tsx
@@ -3,6 +3,9 @@
 import PostSingle from "@/app/post/components/post";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
+import { useEffect } from "react";
+
+import Head from "next/head";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
@@ -28,38 +31,51 @@ const Post = () => {
     await queryClient.refetchQueries({ queryKey: ["post"] });
   };
 
+  useEffect(() => {
+    document.title = `${post?.user?.username} on Twitter: ${post?.content}`;
+  }, []);
+
   return (
-    <div className="lg:w-[600px] border-l border-r border-l-border border-r-border h-full relative">
-      {/* FEED CONTROL UI */}
-      <div className="flex backdrop-blur-lg absolute top-0 w-full">
-        <div className="bg-transparent flex-1 p-2 border-b border-b-border font-bold flex items-center gap-12">
-          <Link
-            className="p-2 rounded-full hover:bg-neutral-500/20 transition-all cursor-pointer"
-            href={"/home"}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+    <>
+      <Head>
+        <title>{document.title}</title>
+        <meta
+          name="description"
+          content="Home page of my attempt to make a clone of Twitter"
+        />
+      </Head>
+      <div className="lg:w-[600px] border-l border-r border-l-border border-r-border h-full relative">
+        {/* FEED CONTROL UI */}
+        <div className="flex backdrop-blur-lg absolute top-0 w-full">
+          <div className="bg-transparent flex-1 p-2 border-b border-b-border font-bold flex items-center gap-12">
+            <Link
+              className="p-2 rounded-full hover:bg-neutral-500/20 transition-all cursor-pointer"
+              href={"/home"}
             >
-              <path d="m12 19-7-7 7-7" />
-              <path d="M19 12H5" />
-            </svg>
-          </Link>
-          <h1>Following</h1>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m12 19-7-7 7-7" />
+                <path d="M19 12H5" />
+              </svg>
+            </Link>
+            <h1>Following</h1>
+          </div>
         </div>
+        <div className="mt-[57.1px]"></div>
+        {post && (
+          <PostSingle post={post} refetchPosts={refetchPosts}></PostSingle>
+        )}
       </div>
-      <div className="mt-[57.1px]"></div>
-      {post && (
-        <PostSingle post={post} refetchPosts={refetchPosts}></PostSingle>
-      )}
-    </div>
+    </>
   );
 };
 
