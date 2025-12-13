ce5ece8c6a9ff523764add6a90a6d91bed9f647a
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2025-12-13 14:01:49 +0800

    refactor(schema): improved the recursive nature of replySchema by using z.lazy() callback

--- apps/frontend/src/schemas/post.ts
+++ apps/frontend/src/schemas/post.ts
@@ -2,55 +2,30 @@ import { z } from "zod";
 
 import { replySchema } from "@/schemas/reply";
 
-const postSchema = z.object({
+export const userSchema = z.object({
+  avatar: z.string(),
+  username: z.string(),
+  name: z.string(),
+  id: z.number(),
+});
+
+export const likeSchema = z.object({
+  userId: z.number(),
+});
+
+export const postSchema = z.object({
   id: z.number(),
   replyId: z.number(),
   userId: z.number(),
   content: z.string(),
   createdAt: z.date(),
   deleted: z.boolean(),
-  user: z.object({
-    avatar: z.string(),
-    username: z.string(),
-    name: z.string(),
-    id: z.number(),
-  }),
+  user: userSchema,
   _count: z.object({
     Like: z.number(),
     replies: z.number(),
   }),
-  Like: z.array(
-    z.object({
-      userId: z.number(),
-    }),
-  ),
+  Like: z.array(likeSchema),
   replies: z.array(replySchema),
-  reply: z
-    .object({
-      id: z.number(),
-      replyId: z.number(),
-      userId: z.number(),
-      content: z.string(),
-      createdAt: z.date(),
-      deleted: z.boolean(),
-      user: z.object({
-        avatar: z.string(),
-        username: z.string(),
-        name: z.string(),
-        id: z.number(),
-      }),
-      _count: z.object({
-        Like: z.number(),
-        replies: z.number(),
-      }),
-      Like: z.array(
-        z.object({
-          userId: z.number(),
-        }),
-      ),
-      replies: z.array(replySchema),
-    })
-    .optional(),
+  reply: replySchema.optional(),
 });
-
-export { postSchema };
--- apps/frontend/src/schemas/reply.ts
+++ apps/frontend/src/schemas/reply.ts
@@ -1,100 +1,22 @@
 import { z } from "zod";
 
-export const replySchema = z.object({
-  id: z.number(),
-  replyId: z.number(),
-  userId: z.number(),
-  content: z.string(),
-  createdAt: z.date(),
-  deleted: z.boolean(),
-  user: z.object({
-    avatar: z.string(),
-    username: z.string(),
-    name: z.string(),
-    id: z.number(),
-  }),
-  _count: z.object({
-    Like: z.number(),
-    replies: z.number(),
-  }),
-  Like: z.array(
-    z.object({
-      userId: z.number(),
-    }),
-  ),
-  replies: z.array(
-    z.object({
-      id: z.number(),
-      replyId: z.number(),
-      userId: z.number(),
-      content: z.string(),
-      createdAt: z.date(),
-      deleted: z.boolean(),
-      user: z.object({
-        avatar: z.string(),
-        username: z.string(),
-        name: z.string(),
-        id: z.number(),
-      }),
-      _count: z.object({
-        Like: z.number(),
-        replies: z.number(),
-      }),
-      Like: z.array(
-        z.object({
-          userId: z.number(),
-        }),
-      ),
-    }),
-  ),
-  reply: z.object({
+import { likeSchema, userSchema } from "@/schemas/post";
+
+export const replySchema: z.ZodType<any> = z.lazy(() =>
+  z.object({
     id: z.number(),
     replyId: z.number(),
     userId: z.number(),
     content: z.string(),
     createdAt: z.date(),
     deleted: z.boolean(),
-    user: z.object({
-      avatar: z.string(),
-      username: z.string(),
-      name: z.string(),
-      id: z.number(),
-    }),
+    user: userSchema,
     _count: z.object({
       Like: z.number(),
       replies: z.number(),
     }),
-    Like: z.array(
-      z.object({
-        userId: z.number(),
-      }),
-    ),
-    replies: z.array(
-      z.array(
-        z.object({
-          id: z.number(),
-          replyId: z.number(),
-          userId: z.number(),
-          content: z.string(),
-          createdAt: z.date(),
-          deleted: z.boolean(),
-          user: z.object({
-            avatar: z.string(),
-            username: z.string(),
-            name: z.string(),
-            id: z.number(),
-          }),
-          _count: z.object({
-            Like: z.number(),
-            replies: z.number(),
-          }),
-          Like: z.array(
-            z.object({
-              userId: z.number(),
-            }),
-          ),
-        }),
-      ),
-    ),
+    Like: z.array(likeSchema),
+    replies: z.array(replySchema), // recursive
+    reply: replySchema.optional(), // recursive optional
   }),
-});
+);
